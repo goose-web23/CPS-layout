59858441ae588cecd0aad2aed9538ba89465bb8b
Надежда Иванова <forstudyni@yandex.ru>
2025-11-23 16:48:30 +0900

    added all modals, hover, color variables

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,12 +1,26 @@
 const callModal = document.getElementById('callModal');
 const messageModal = document.getElementById('messageModal');
-const overlay = document.getElementById('burger-overlay');
+const userModal = document.getElementById('userModal');
+const toolsModal = document.getElementById('toolsModal');
+const searchModal = document.getElementById('searchModal');
 
+const overlay = document.getElementById('burger-overlay');
 const closeButtons = document.querySelectorAll('.modal__close');
 
 document.getElementById('call-btn').addEventListener('click', openModal.bind(null, callModal));
 document.getElementById('call-btn-burger').addEventListener('click', openModal.bind(null, callModal));
+
 document.getElementById('message-btn').addEventListener('click', openModal.bind(null, messageModal));
+document.getElementById('message-btn-burger').addEventListener('click', openModal.bind(null, messageModal));
+
+document.getElementById('user-btn').addEventListener('click', openModal.bind(null, userModal));
+document.getElementById('user-btn-burger').addEventListener('click', openModal.bind(null, userModal));
+
+document.getElementById('tools-btn').addEventListener('click', openModal.bind(null, toolsModal));
+document.getElementById('tools-btn-services').addEventListener('click', openModal.bind(null, toolsModal));
+
+document.getElementById('search-btn').addEventListener('click', openModal.bind(null, searchModal));
+document.getElementById('search-btn-services').addEventListener('click', openModal.bind(null, searchModal));
 
 closeButtons.forEach(btn => {
     btn.addEventListener('click', closeAllModals);
@@ -23,6 +37,9 @@ function openModal(modal) {
 function closeAllModals() {
     callModal.classList.remove('open');
     messageModal.classList.remove('open');
+    userModal.classList.remove('open');
+    toolsModal.classList.remove('open');
+    searchModal.classList.remove('open');
     overlay.style.display = 'none';
     document.documentElement.style.overflow = '';
 }
